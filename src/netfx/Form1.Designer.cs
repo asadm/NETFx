d0fba4626c47c89ade4e90985c86d2a136b59d8f
asadlionpk <asad_lionpk@hotmail.com>
2013-01-09 13:00:53 +0500

    implemented vignette and added some more sample effects

--- src/netfx/Form1.Designer.cs
+++ src/netfx/Form1.Designer.cs
@@ -35,7 +35,8 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -44,7 +45,7 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(12, 54);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(448, 531);
+            this.pictureBox1.Size = new System.Drawing.Size(519, 531);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -56,7 +57,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(147, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(129, 36);
+            this.button1.Size = new System.Drawing.Size(99, 36);
             this.button1.TabIndex = 1;
             this.button1.Text = "Batch Effects 1";
             this.button1.UseVisualStyleBackColor = true;
@@ -64,9 +65,9 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(487, 54);
+            this.pictureBox2.Location = new System.Drawing.Point(569, 54);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(426, 531);
+            this.pictureBox2.Size = new System.Drawing.Size(513, 531);
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
             // 
@@ -82,7 +83,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(282, 12);
+            this.button3.Location = new System.Drawing.Point(653, 12);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(129, 36);
             this.button3.TabIndex = 4;
@@ -92,7 +93,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(417, 12);
+            this.button4.Location = new System.Drawing.Point(788, 12);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(129, 36);
             this.button4.TabIndex = 5;
@@ -100,23 +101,33 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // label1
+            // button5
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(591, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(74, 13);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "LOADING...";
+            this.button5.Location = new System.Drawing.Point(252, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(93, 36);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Batch Effects 2";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(351, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(97, 36);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Batch Effects 3";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1088, 624);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -130,7 +141,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -143,7 +153,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
